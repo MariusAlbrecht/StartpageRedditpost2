d53313f383e3bd9fae03f6607aa2d8a48f708630
mxrixs <64852250+mxrixs@users.noreply.github.com>
2021-08-28 17:16:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Startpage3
+
+### Image
+![image](https://cdn.discordapp.com/attachments/684107329427472406/879836842910949416/unknown.png)
+
+
+### Stuff used
+
+#### Icons
+
+* [dripicons-weather](https://github.com/amitjakhu/dripicons-weather)
+	
+#### Weather API
+
+* [Openweathermap](https://openweathermap.org/)
+	
+#### Color Theme
+
+* [nord](https://www.nordtheme.com/)
+
+### How to use yourslef
+
+* clone/fork repo
+* adjust weather-api config in the following way:
+  * create an account for openweathermap.org [here](https://home.openweathermap.org/users/sign_up)
+  * get your API key and insert it under `const apikey` in `script.js`
+  * get your city ID from the list of cities one can download [here](https://bulk.openweathermap.org/sample/city.list.json.gz) and insert it under `cont citiname` in `script.js`
+* possibly adjust colours by changing the `vars.css` file or creating an entirely new one
+* host in whatever way
---
+++ assets/script.js
@@ -0,0 +1,84 @@
+function weather(){
+    console.log("weather() has been called");
+    const apikey = "A D D - Y O U R - A P I K E Y - H E R E"
+    const cityID = "A D D - Y O U R - C I T Y - I D - H E R E"
+    var call = 'https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apikey + '&units=metric';
+    fetch(call).then(res => res.json()).then(handler);
+}
+weather();
+
+function clock() {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    
+    hrs = hrs < 10 ? `0${hrs}` : hrs;
+    mins = mins < 10 ? `0${mins}` : mins;
+    
+    let time = `${hrs}:${mins}`;
+    setInterval(clock, 1000);
+    document.getElementById("clock").innerText = time;
+}
+clock();
+    
+function handler(res){
+    
+    console.log("starting...");
+    const temperature = res.main.temp;
+    console.log(temperature);
+    const identifier = res.weather[0].id;
+    console.log(identifier);
+    
+    
+    console.log("using if...");
+    
+    if(identifier == 200 || identifier == 201 || identifier == 202){
+        document.getElementById("Icon").src = "assets/icons/cloud-rain-lightning.svg";
+    }
+    else if(identifier == 210 || identifier == 211 || identifier == 212 || identifier == 221){
+        document.getElementById("Icon").src = "assets/icons/cloud-lightning.svg";
+    }
+    else if(identifier == 210 || identifier == 211 || identifier == 212 || identifier == 221){
+        document.getElementById("Icon").src = "assets/icons/cloud-lightning.svg";
+    }
+    else if(identifier == 230 || identifier == 231 || identifier == 232){
+        document.getElementById("Icon").src = "assets/icons/cloud-drizzle-lightning.svg";
+    }
+    else if(identifier == 300 || identifier == 301 || identifier == 302 || identifier == 310 || identifier == 311 || identifier == 312 || identifier == 313 || identifier == 314 || identifier == 321){
+        document.getElementById("Icon").src = "assets/icons/cloud-drizzle-lightning.svg";
+    }
+    else if(identifier == 500 || identifier == 501 || identifier == 520 || identifier == 521){
+        document.getElementById("Icon").src = "assets/icons/cloud-rain.svg";
+    }
+    else if(identifier == 502 || identifier == 503 || identifier == 504 || identifier == 520 || identifier == 521|| identifier == 522|| identifier == 531){
+        document.getElementById("Icon").src = "assets/icons/cloud-rain-2.svg";
+    }
+    else if(identifier == 600 || identifier == 601|| identifier == 602|| identifier == 611 || identifier == 612 || identifier == 613|| identifier == 615|| identifier == 616|| identifier == 620|| identifier == 621|| identifier == 622){
+        document.getElementById("Icon").src = "assets/icons/cloud-snow.svg";
+    }
+    else if(identifier == 701 || identifier == 711 || identifier == 721 || identifier == 731 || identifier == 741 || identifier == 751 || identifier == 761 || identifier == 762){
+        document.getElementById("Icon").src = "assets/icons/fog.svg";
+    }
+    else if(identifier == 771){
+        document.getElementById("Icon").src = "assets/icons/wind.svg";
+    }
+    else if(identifier == 781){
+        document.getElementById("Icon").src = "assets/icons/tornado.svg";
+    }
+    else if(identifier == 800){
+        document.getElementById("Icon").src = "assets/icons/sun.svg";
+    }
+    else if(identifier == 801 || identifier == 802){
+        document.getElementById("Icon").src = "assets/icons/cloud.svg";
+    }
+    else if(identifier == 803 || identifier == 804){
+        document.getElementById("Icon").src = "assets/icons/clouds.svg";
+    }
+    else{
+        document.getElementById("Icon").src = "assets/icons/error.svg"
+    }
+
+    console.log("temp is...");
+
+    document.getElementById("temperature").innerText = temperature + '°';
+}
